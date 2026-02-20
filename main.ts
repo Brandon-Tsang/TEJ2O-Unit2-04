4ab8787c96a431ada6f108dad73ca0933b8fa622
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-02-20 13:03:27 -0500

    Shows the temperature now

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Brandon
+ * Created on: Feb 2026
+ * This program will show me the temperature.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen ()
+basic.showIcon (IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    let currentTemp = input.temperature();
+    basic.showString("The Temperature is: " + convertToText(currentTemp) + " C.")
+})
